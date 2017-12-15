814f82f82a2080edde731c22eef46468c7f6e9ae
phwebmachine <phwebmachine@gmail.com>
2017-12-15 13:56:31 +0800

    feat: make all prime-factors test pass

--- cpp/prime-factors/prime_factors.h
+++ cpp/prime-factors/prime_factors.h
@@ -4,6 +4,19 @@ namespace prime_factors
 {
     std::vector<int> of (int input)
     {
-        return {};
+        std::vector<int> prime_factor_vector;
+
+        for (int i=2; i<=input; i++){
+            if (input % i == 0){
+                prime_factor_vector.push_back(i);
+                input = input/i;
+                std::vector<int> recursive_factors = of(input);
+                // vector concatenation https://stackoverflow.com/a/3177254
+                prime_factor_vector.insert(prime_factor_vector.end(), recursive_factors.begin(), recursive_factors.end());
+                break;
+            };
+        };
+
+        return prime_factor_vector;
     };
 };
