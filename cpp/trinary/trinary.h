78fe53d5769eccd4750d371c9cd9f1ccec9e8c5c
phwebmachine <phwebmachine@gmail.com>
2017-12-18 18:05:28 +0800

    feat: update trinary code comments

--- cpp/trinary/trinary.h
+++ cpp/trinary/trinary.h
@@ -6,7 +6,7 @@ namespace trinary {
 		int decimal_value = 0;
 		for (int i=input.length() - 1; i >=0 ; i--){
 			char current_char = input.at(i);
-			// if the current char is neither a 0 or a 1, break and just return 0
+			// if the current char is neither a 0, 1, or 2, break and just return 0
 			if ((current_char != '0') && (current_char != '1') && (current_char != '2')){
 				return 0;
 			};
