7786bb929baed04b79e655f343865ae8bb1d74a8
phwebmachine <phwebmachine@gmail.com>
2017-12-18 23:35:01 +0800

    feat: make series problem pass digits tests

--- cpp/series/series_test.cpp
+++ cpp/series/series_test.cpp
@@ -15,7 +15,6 @@ BOOST_AUTO_TEST_CASE(short_digits)
     BOOST_TEST(expected == actual, tt::per_element());
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(long_digits)
 {
     const vector<int> expected{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -42,6 +41,7 @@ BOOST_AUTO_TEST_CASE(keeps_arbitrary_digit_order)
 
     BOOST_TEST(expected == actual, tt::per_element());
 }
+#if defined(EXERCISM_RUN_ALL_TESTS)
 
 BOOST_AUTO_TEST_CASE(can_slice_by_1)
 {
