13a5709dc8b75310bb234ebf70ec5b6592f940d5
phwebmachine <phwebmachine@gmail.com>
2017-12-21 14:20:31 +0800

    refactor: simplify code by reducing by using string::substr instead

--- cpp/series/series.h
+++ cpp/series/series.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+
 namespace series
 {
     std::vector<int> digits(std::string digits_string)
@@ -11,7 +12,7 @@ namespace series
             // cast char to int
             int elem = digits_string.at(i);
             // subtract 48 to transform value from char to int
-            integer_vector.push_back(elem - 48);
+            integer_vector.push_back(elem - '0');
         };
 
         return integer_vector;
@@ -28,23 +29,21 @@ namespace series
 
         // loop through each and every item in the digits_string
         for (int i=0; i<=digits_string.length() - series_length; i++){
-            std::vector<int> current_vector;
-            // loop through each succeeding digit up to the indicated series_length
-            for (int k=0; k < series_length; k++){
-                // continue looping as long as we do not exceed the length of the original digits_string
-                if (k + i <= digits_string.length()){
-                    // convert each char into an int, and add it to the current_vector we are building
-                    current_vector.push_back(digits_string.at(i+k) - 48);
-                // if we have exceeded the length of the original digits_string, then we break the for loop
-                }else{
-                    break;
-                };
 
-                // as long as the current_vector size matches the desired series length, then we add it to
-                // the result
-                if (current_vector.size() == series_length){
-                    result_vector.push_back(current_vector);
-                };
+            // get the substring in question
+            std::string substring;
+            substring = digits_string.substr(i, series_length);
+
+            // converte each char from the substring into an int, and append it onto the current_vector container
+            std::vector<int> current_vector;
+            for (int k=0; k<substring.length(); k++){
+                current_vector.push_back(substring.at(k) - '0');
+            };
+
+            // as long as the current_vector size matches the desired series length, then we add it to
+            // the result
+            if (current_vector.size() == series_length){
+                result_vector.push_back(current_vector);
             };
         };
 
