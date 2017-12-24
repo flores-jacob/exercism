f9f4bdd2a72c26c141710d73ee0ef190b485f6f3
phwebmachine <phwebmachine@gmail.com>
2017-12-25 02:12:47 +0800

    feat: enable handling of upper case input

--- cpp/atbash-cipher/atbash_cipher.h
+++ cpp/atbash-cipher/atbash_cipher.h
@@ -7,9 +7,16 @@ namespace atbash
 
     std::string encode(std::string input_string)
     {
+        std::string input_lower_case;
+
+        // convert the input string to lower case
+        for (char elem: input_string){
+            input_lower_case += std::tolower(elem);
+        };
+
         std::string output_string;
 
-        for (char elem:input_string){
+        for (char elem: input_lower_case){
             int pos = plain_text.find(elem);
             output_string += cipher_text.at(pos);
         };
