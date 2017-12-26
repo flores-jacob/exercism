844299b6d1d5d1e53a09428708166e45c01be3e8
phwebmachine <phwebmachine@gmail.com>
2017-12-27 07:29:38 +0800

    docs: add comment to leap.py

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,3 +1,5 @@
 def is_leap_year(year):
+    # if the year is divisible by 4 AND if it's either not divisible by 100 or divisible by 400,
+    # then it's a leap year
     return (year % 4 == 0) and (not (year % 100 == 0) or (year % 400 == 0))
 
