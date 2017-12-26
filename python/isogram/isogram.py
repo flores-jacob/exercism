9b3cce507cb5ddc6907f5ffd4d002734230e7517
phwebmachine <phwebmachine@gmail.com>
2017-12-27 07:44:27 +0800

    feat: implement isogram.py and pass all tests

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,11 @@
 def is_isogram(string):
-    pass
+    is_isogram = True
+
+    string = string.lower()
+
+    for letter in string:
+        if (letter != "-") and (letter != ' '):
+            if string.count(letter) > 1:
+                is_isogram = False
+
+    return is_isogram
