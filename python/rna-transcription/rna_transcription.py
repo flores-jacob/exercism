36ca1a505cdb1636af13d4d7c80e124abd98eac2
phwebmachine <phwebmachine@gmail.com>
2017-12-27 08:01:05 +0800

    feat: implement rna_transcription.py and pass all tests

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,16 @@
 def to_rna(dna_strand):
-    pass
+    rna_strand = ''
+
+    for nucleotide in dna_strand:
+        if nucleotide == 'C':
+            rna_strand += 'G'
+        elif nucleotide == 'G':
+            rna_strand += 'C'
+        elif nucleotide == 'T':
+            rna_strand += 'A'
+        elif nucleotide == 'A':
+            rna_strand += 'U'
+        else:
+            raise ValueError(nucleotide + ' is an invalid input')
+
+    return rna_strand
