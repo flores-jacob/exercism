566dffedc89122dce31988f7add996d68a24cd59
phwebmachine <phwebmachine@gmail.com>
2017-12-28 14:41:24 +0800

    feat: handle special characters and apostrophe

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,10 +1,11 @@
 def word_count(phrase):
-    phrase = phrase.replace(',', ' ')
 
-    print (phrase)
+    trans_dict = phrase.maketrans(',:!@$%^&._', '          ')
+    phrase = phrase.translate(trans_dict).lower()
 
     result_dict = {}
     words = phrase.split()
+    words = [word.strip("'") for word in words]
     for word in words:
         result_dict[word] = words.count(word)
 
