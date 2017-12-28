daacd4cf2519d787a1578c7d46bd53caaa6cfdd6
phwebmachine <phwebmachine@gmail.com>
2017-12-28 14:09:43 +0800

    feat: implement hamming python exercise and pass all tests

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,12 @@
 def distance(strand_a, strand_b):
-    pass
+    ham_distance = 0
+
+    if len(strand_a) != len(strand_b):
+        raise ValueError("strand length mismatch")
+
+    for i in range(len(strand_a)):
+        # print(strand_a[i], strand_b[i])
+        if strand_a[i] != strand_b[i]:
+            ham_distance += 1
+
+    return ham_distance
