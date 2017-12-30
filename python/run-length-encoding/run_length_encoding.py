fcf960ab18c1b73cb864a9d98db217ef65062d4c
phwebmachine <phwebmachine@gmail.com>
2017-12-30 10:05:34 +0800

    refactor: reimplement to use a for loop instead of a dict

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -6,19 +6,29 @@ def decode(string):
 
 def encode(string):
     encoded_string = ''
-    character_dict = OrderedDict()
 
     string_list = list(string)
+    previous_char = None
+    current_count = 0
     for character in string_list:
-        if character in character_dict.keys():
-            character_dict[character] += 1
+        if not previous_char:
+            current_count += 1
+        elif character == previous_char:
+            current_count += 1
         else:
-            character_dict[character] = 1
+            if current_count > 1:
+                encoded_string = encoded_string + str(current_count)
+            encoded_string = encoded_string + previous_char
+            current_count = 1
+        previous_char = character
 
-    for character_key in character_dict:
-        count = character_dict[character_key]
-        if count > 1:
-            encoded_string += str(count)
-        encoded_string += character_key
+    # append the final char and count
+    if current_count > 1:
+        encoded_string += str(current_count)
+    encoded_string += previous_char
+
 
     return encoded_string
+
+
+# mystring = encode('AABBBCCCC'), '2A3B4C'
