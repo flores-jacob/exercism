a3c539733f6d5e1c4d8c2020dee4954699be0607
phwebmachine <phwebmachine@gmail.com>
2017-12-30 09:41:21 +0800

    feat: enable run_length_encoding.py to pass first 3 tests

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,24 @@
+from collections import OrderedDict
+
 def decode(string):
     pass
 
 
 def encode(string):
-    pass
+    encoded_string = ''
+    character_dict = OrderedDict()
+
+    string_list = list(string)
+    for character in string_list:
+        if character in character_dict.keys():
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
+
+    for character_key in character_dict:
+        count = character_dict[character_key]
+        if count > 1:
+            encoded_string += str(count)
+        encoded_string += character_key
+
+    return encoded_string
