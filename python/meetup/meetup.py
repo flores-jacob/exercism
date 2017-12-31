a0f4920f0e867180033587c1b8438820dc2a2890
phwebmachine <phwebmachine@gmail.com>
2018-01-01 01:40:45 +0800

    feat: Implement teenth day of the week

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,2 +1,9 @@
+from datetime import date
+import calendar
+
 def meetup_day(year, month, day_of_the_week, which):
-    pass
+
+    for day in range(13, 20):
+        teenth_date = date(year, month, day)
+        if calendar.day_name[teenth_date.weekday()] == day_of_the_week:
+            return teenth_date
