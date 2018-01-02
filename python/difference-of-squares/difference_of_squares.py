e76f7f69f71cc70d073595eb2d07e738671530e0
phwebmachine <phwebmachine@gmail.com>
2018-01-02 09:47:36 +0800

    refactor: Reduce number of computations performed functions

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,9 +1,9 @@
 def square_of_sum(value):
-    return sum([i + 1 for i in range(value)]) ** 2
+    return sum([i for i in range(1, value + 1)]) ** 2
 
 
 def sum_of_squares(value):
-    return sum([(i + 1) ** 2 for i in range(value)])
+    return sum([i ** 2 for i in range(1, value + 1)])
 
 
 def difference(value):
