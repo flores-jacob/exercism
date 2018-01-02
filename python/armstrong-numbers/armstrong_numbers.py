d9ccb1f7e4f8d8a781178a29351cf1cbe116e79a
phwebmachine <phwebmachine@gmail.com>
2018-01-02 09:09:45 +0800

    feat: Implement armstrong_numbers.py and pass all tests

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,10 @@
 def is_armstrong(number):
-    pass
+    number_str = str(number)
+    armstrong_sum = 0
+    for number_char in number_str:
+        armstrong_sum += int(number_char) ** len(number_str)
+
+    if armstrong_sum == number:
+        return True
+    else:
+        return False
