894d2466a094c5a1e60af3ffd2a113c30c97f0cc
phwebmachine <phwebmachine@gmail.com>
2018-01-02 09:41:03 +0800

    feat: Implement difference_of_squares function and pass all tests

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -6,5 +6,5 @@ def sum_of_squares(value):
     return sum([(i + 1) ** 2 for i in range(value)])
 
 
-def difference():
-    pass
+def difference(value):
+    return square_of_sum(value) - sum_of_squares(value)
