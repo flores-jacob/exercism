0f7304cce9df36aa9c62cd67903e94c810412b51
phwebmachine <phwebmachine@gmail.com>
2018-01-02 09:38:48 +0800

    feat: Implement sum_of_squares function

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -2,8 +2,8 @@ def square_of_sum(value):
     return sum([i + 1 for i in range(value)]) ** 2
 
 
-def sum_of_squares():
-    pass
+def sum_of_squares(value):
+    return sum([(i + 1) ** 2 for i in range(value)])
 
 
 def difference():
