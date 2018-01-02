4b5bb2b1ae52c86c3efd4f89a04f788219004a40
phwebmachine <phwebmachine@gmail.com>
2018-01-02 09:31:19 +0800

    feat: Implement rotational_cipher.py and pass all tests

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,19 @@
 def rotate(text, key):
-    pass
+    special_chars = ". ,'\"!?1234567890"
+    plain_key_base = "abcdefghijklmnopqrstuvwxyz"
+    cipher_key_base = plain_key_base[key:] + plain_key_base[:key]
+
+    plain_key = plain_key_base + special_chars
+    cipher_key = cipher_key_base + special_chars
+
+    cipher_text = ""
+
+    for char in text:
+        lowered_char = char.lower()
+        index = plain_key.index(lowered_char)
+        if char.isupper():
+            cipher_char = cipher_key[index].upper()
+        else:
+            cipher_char = cipher_key[index]
+        cipher_text += cipher_char
+    return cipher_text
