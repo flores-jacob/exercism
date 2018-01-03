92022e47f68d102740a5e69270fa4ddf04f47eb5
phwebmachine <phwebmachine@gmail.com>
2018-01-03 08:59:49 +0800

    feat: Implement anagram python exercise and pass all tests

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,10 @@
 def detect_anagrams(word, candidates):
-    pass
+    anagram_list =[]
+    letter_sorted_word = sorted(word.lower())
+    for candidate in candidates:
+        candidate_sorted_word = sorted(candidate.lower())
+        if candidate_sorted_word == letter_sorted_word:
+            if word.lower() != candidate.lower():
+                anagram_list.append(candidate)
+
+    return anagram_list
