e9bd39dcc0e51af808c3f879730efdda9cd8be7f
phwebmachine <phwebmachine@gmail.com>
2018-01-03 10:28:35 +0800

    feat: Implement series.py and pass all tests

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,13 @@
 def slices(series, length):
-    pass
+    series_list = []
+
+    if length == 0:
+        raise ValueError("Cannot have a length of 0")
+    elif len(series) < length:
+        raise ValueError("Desired length exceeds series length")
+
+    for index in range(len(series)):
+        appendable_list = [int(x) for x in series[index:index + length]]
+        if len(appendable_list) == length:
+            series_list.append(appendable_list)
+    return series_list
