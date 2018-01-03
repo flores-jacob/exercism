43e9c1cb524f92e5c36f10145716c8e0451faf65
phwebmachine <phwebmachine@gmail.com>
2018-01-03 10:05:26 +0800

    feat: Implement allergies.py and pass all tests

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,22 @@
+import math
+
+
 class Allergies(object):
+    allergen_masterlist = ["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"]
 
     def __init__(self, score):
-        pass
+        self.score = score
+        self.allergy_list = []
+
+        while score:
+            allergen_index = int(math.floor(math.log(score, 2)))
+            if allergen_index < len(Allergies.allergen_masterlist):
+                self.allergy_list.append(Allergies.allergen_masterlist[allergen_index])
+            score -= pow(2, allergen_index)
 
     def is_allergic_to(self, item):
-        pass
+        return item in self.allergy_list
 
     @property
     def lst(self):
-        pass
+        return self.allergy_list
