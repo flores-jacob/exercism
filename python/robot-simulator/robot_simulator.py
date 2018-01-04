bd5779716e6ae6b53bc66dfb91260ccf82fb258d
phwebmachine <phwebmachine@gmail.com>
2018-01-04 15:01:53 +0800

    feat: Enable robot initialization and turning

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -1,11 +1,22 @@
 # Globals for the bearings
 # Change the values as you see fit
-EAST = None
-NORTH = None
-WEST = None
-SOUTH = None
+EAST = 1
+NORTH = 0
+WEST = 3
+SOUTH = 2
 
 
 class Robot(object):
     def __init__(self, bearing=NORTH, x=0, y=0):
-        pass
+        self.bearing = bearing
+        self.coordinates = (x, y)
+
+    def turn_right(self):
+        self.bearing += 1
+        if self.bearing > 3:
+            self.bearing = 0
+
+    def turn_left(self):
+        self.bearing -= 1
+        if self.bearing < 0:
+            self.bearing = 3
