5b011d9e80b647c14aac80b246ccccfa6c2dccd5
phwebmachine <phwebmachine@gmail.com>
2018-01-09 22:51:50 +0800

    feat: Implement encode function

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,5 +1,23 @@
+number_key = "1234567890"
+base_key = "abcdefghijklmnopqrstuvwxyz"
+plain_key = base_key + number_key
+cipher_key = base_key[::-1] + number_key
+
+
 def encode(plain_text):
-    pass
+    plain_text = filter(str.isalnum, plain_text.lower())
+
+    cipher_text = ""
+
+    count = 0
+    for i in plain_text:
+        count += 1
+        index = plain_key.index(i)
+        cipher_text += cipher_key[index]
+        if count % 5 == 0:
+            cipher_text += ' '
+
+    return cipher_text.strip()
 
 
 def decode(ciphered_text):
