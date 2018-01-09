f0394f2ebded1f4181fe0f3392486a1a8effc1b8
phwebmachine <phwebmachine@gmail.com>
2018-01-09 22:35:32 +0800

    feat: Implement sieve.py and pass all tests

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,18 @@
 def sieve(limit):
-    pass
+    
+    desired_range = range(2, limit + 1)
+    
+    not_primes = set()
+
+    for current_candidate in desired_range:
+        if current_candidate in not_primes:
+            continue
+
+        for succeeding_num in desired_range:
+            if (succeeding_num % current_candidate == 0) and (current_candidate != succeeding_num):
+                not_primes.add(succeeding_num)
+
+    prime_list = sorted(list(set(desired_range) - not_primes))
+
+    return prime_list
+
