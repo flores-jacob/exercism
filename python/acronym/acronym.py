921968aa2b8bc3c98280943500f56cdac63113fe
phwebmachine <phwebmachine@gmail.com>
2018-01-09 23:31:47 +0800

    feat: Implement abbreviate and pass all tests

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,7 @@
 def abbreviate(words):
-    pass
+    words = words.replace('-', ' ')
+    word_list = words.split()
+
+    acronym_letter_list = [word[0].upper() for word in word_list]
+
+    return "".join(acronym_letter_list)
