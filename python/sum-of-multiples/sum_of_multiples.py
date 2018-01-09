03921b116fc784b2f22ebe4dc1e78ae76a8cb11e
phwebmachine <phwebmachine@gmail.com>
2018-01-09 23:15:39 +0800

    feat: Implement sum_of_multiples.py and pass all tests

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,9 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    multiple_list = []
+    for i in range(1, limit):
+        for multiple in multiples:
+            if i % multiple == 0:
+                multiple_list.append(i)
+                break
+
+    return sum(multiple_list)
