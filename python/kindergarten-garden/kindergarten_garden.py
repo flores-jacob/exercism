a1178c5a2906f5d9e5bdaadf21327887ac98f978
phwebmachine <phwebmachine@gmail.com>
2018-01-11 22:35:26 +0800

    feat: Implement kindergarten_garden.py and pass all tests

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,17 @@
 class Garden(object):
-    def __init__(self, diagram, students):
-        pass
+    students = sorted([ "Alice", "Bob", "Charlie", "David",
+                "Eve", "Fred", "Ginny", "Harriet",
+                "Ileana", "Joseph", "Kincaid", "Larry"])
+
+    plant_names_dict = {"V": "Violets", "R": "Radishes", "C": "Clover", "G": "Grass"}
+
+    def __init__(self, diagram, students=students):
+        self.student_list = sorted(students)
+        self.top_plants, self.bottom_plants = diagram.split()
+
+    def plants(self, student_name):
+        student_index = self.student_list.index(student_name)
+        plant_index = student_index * 2
+        students_plants = self.top_plants[plant_index: plant_index + 2] + self.bottom_plants[plant_index: plant_index + 2]
+
+        return[Garden.plant_names_dict[plant_letter] for plant_letter in students_plants]
