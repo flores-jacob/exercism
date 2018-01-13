4f24d558cc42fc5fa7b9c3a507d7ecb921639e5f
phwebmachine <phwebmachine@gmail.com>
2018-01-13 14:15:05 +0800

    feat: Implement flatten_array.py

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,13 @@
 def flatten(iterable):
-    pass
+
+    if type(iterable) is list:
+        if len(iterable) > 0:
+            return flatten(iterable[0]) + (flatten(iterable[1:]))
+        else:
+            return []
+    else:
+        if (iterable is not None) and (iterable != ()):
+            return [iterable]
+        else:
+            return []
+
