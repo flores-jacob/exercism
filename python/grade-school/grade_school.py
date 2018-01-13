a862e995b57af3bb473a02c254d0a0a4718f0116
phwebmachine <phwebmachine@gmail.com>
2018-01-13 12:56:42 +0800

    feat: Enable school initialization and grade method

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,3 +1,10 @@
 class School(object):
     def __init__(self, name):
-        pass
+        self.name = name
+        self.roster = {}
+
+        for i in range(1, 10):
+            self.roster[i] = set()
+
+    def grade(self, grade_num):
+        return self.roster[grade_num]
