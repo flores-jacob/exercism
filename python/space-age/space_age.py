a773eefa5b5c5c71f5879969ae6f56754864b88c
phwebmachine <phwebmachine@gmail.com>
2018-01-14 15:07:57 +0800

    feat: Implement on_mercury method

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,2 +1,7 @@
 class SpaceAge(object):
-    pass
+    def __init__(self, seconds):
+        self.age_in_seconds = seconds
+        self.earth_age = seconds/31557600
+
+    def on_mercury(self):
+        return round(self.earth_age/0.2408467, 2)
