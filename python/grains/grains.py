77ecf3db7b27181893a69b901d9e8d3de876ce38
phwebmachine <phwebmachine@gmail.com>
2018-01-15 08:33:47 +0800

    feat: Implement total_after and pass all tests

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -11,3 +11,10 @@ def on_square(integer_number):
 def total_after(integer_number):
     if (integer_number < 1) or (integer_number > 64):
         raise ValueError("input square should be between 1 and 64")
+
+    total_grains = 0
+
+    for i in range(1, integer_number + 1):
+        total_grains += on_square(i)
+
+    return total_grains
