c6c30d9dfea80502ada111969d55809a46006f6c
phwebmachine <phwebmachine@gmail.com>
2018-01-17 11:36:02 +0800

    refactor: Simplify code with list comprehension

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -2,20 +2,18 @@ def verify(isbn):
     # Remove any dashes
     no_dashes = isbn.replace("-", "")
     # Make sure that string length is correct
-    if len (no_dashes) != 10:
+    if len(no_dashes) != 10:
         return False
 
     initial_nine_nums = no_dashes[:9]
     final_num = no_dashes[9]
 
-    total = 0
-    # Loop through the first 9 numbers, multiply by their factors, and
-    # add to the total if they are digits
-    for i in range(9):
-        if initial_nine_nums[i].isdigit():
-            total += int(initial_nine_nums[i]) * (10 - i)
-        else:
-            return False
+    # Ensure that all chars are digits
+    if not initial_nine_nums.isdigit():
+        return False
+
+    # Get sum of first nine numbers
+    total = sum([int(num) * (10 - index) for index, num in enumerate(initial_nine_nums)])
 
     # Add the correct value for the 10th and last digit
     if final_num == 'X':
