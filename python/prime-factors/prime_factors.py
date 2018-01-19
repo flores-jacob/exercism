a7ab29d18daaeba07c149d2cb5a1692861b479b2
phwebmachine <phwebmachine@gmail.com>
2018-01-20 03:58:56 +0800

    feat: Implement prime_factors.py

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,12 @@
 def prime_factors(natural_number):
-    pass
+    factor_list = []
+    candidate_factor = 2
+
+    while candidate_factor <= natural_number:
+        if natural_number % candidate_factor == 0:
+            factor_list.append(candidate_factor)
+            natural_number /= candidate_factor
+        else:
+            candidate_factor += 1
+
+    return factor_list
