ff5066320953a5a227db00de667342904739ac0a
phwebmachine <phwebmachine@gmail.com>
2018-01-22 08:16:05 +0800

    feat: Implement to pass all tests

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -8,21 +8,31 @@ def translate(text):
     xr_yt_special_cases = ["xr", "yt"]
     three_letter_special_cases = ["thr", "sch"]
 
-    return_text = ""
+    return_text = []
 
     for word in split_text:
         word = word.lower()
         if (word[0] in consonants) and (word[1:3] == "qu"):
-            return_text += (word[3:] + word[0:3] + "ay")
+            return_text.append(word[3:] + word[0:3] + "ay")
         elif word[0:3] in three_letter_special_cases:
-            return_text += word[3:] + word[0:3] + "ay"
+            return_text.append(word[3:] + word[0:3] + "ay")
         elif word[0:2] in two_letter_special_cases:
-            return_text += (word[2:] + word[0:2] + "ay")
+            return_text.append(word[2:] + word[0:2] + "ay")
         elif word[0:2] in xr_yt_special_cases:
-            return_text += (word + "ay")
+            return_text.append(word + "ay")
         elif word[0] in vowels:
-            return_text += (word + "ay")
+            return_text.append(word + "ay")
+        elif word[0] == "y":
+            return_text.append(word[1:] + "yay")
         elif word[0] in consonants:
-            return_text += (word[1:] + word[0] + "ay")
+            # identify initial consonant cluster
+            consonant_cluster = ""
+            for letter in word:
+                if (letter not in vowels) and (letter != "y"):
+                    consonant_cluster += letter
+                else:
+                    break
 
-    return return_text
+            return_text.append(word[len(consonant_cluster):] + consonant_cluster + "ay")
+
+    return " ".join(return_text)
