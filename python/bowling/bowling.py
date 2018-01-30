055412b7608c270731f3b1e75a8a1edf4b6eb848
phwebmachine <phwebmachine@gmail.com>
2018-01-30 20:06:09 +0800

    refactor: Replace frame index 9 with FINAL_FRAME for readability

--- python/bowling/bowling.py
+++ python/bowling/bowling.py
@@ -3,6 +3,8 @@ SPARE = "spare"
 OPEN = "open"
 
 
+FINAL_FRAME = 9
+
 class BowlingGame(object):
     def __init__(self):
         self.frames = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -20,7 +22,7 @@ class BowlingGame(object):
         elif pins > 10:
             raise ValueError("Rolls cannot have a score greater than 10")
 
-        if self.current_frame > 9:
+        if self.current_frame > FINAL_FRAME:
             raise IndexError("Cannot roll if there are already 10 frames")
 
         self.roll_count += 1
@@ -33,13 +35,13 @@ class BowlingGame(object):
             self.current_roll = STRIKE
         elif (self.frames[self.current_frame] == 10) and (self.roll_count == 2):
             self.current_roll = SPARE
-        elif (pins == 10) and (self.roll_count == 2) and (self.current_frame == 9):
+        elif (pins == 10) and (self.roll_count == 2) and (self.current_frame == FINAL_FRAME):
             self.current_roll = STRIKE
         else:
             self.current_roll = OPEN
 
-        # For frames 0 to 8, and the first roll of 9, the scoring is the same
-        if (self.current_frame < 9) or ((self.current_frame == 9) and (self.roll_count == 1)):
+        # For frames 0 to 8, and the first roll of FINAL_FRAME, the scoring is the same
+        if (self.current_frame < FINAL_FRAME) or ((self.current_frame == FINAL_FRAME) and (self.roll_count == 1)):
             if self.the_roll_before == STRIKE:
                 self.frames[self.current_frame - 2] += pins
 
@@ -47,12 +49,12 @@ class BowlingGame(object):
                 self.frames[self.current_frame - 1] += pins
             elif self.previous_roll == STRIKE:
                 self.frames[self.current_frame - 1] += pins
-        # However, on the second roll of 9, the scoring is different
-        elif (self.current_frame == 9) and (self.roll_count == 2):
+        # However, on the second roll of FINAL_FRAME, the scoring is different
+        elif (self.current_frame == FINAL_FRAME) and (self.roll_count == 2):
             if self.the_roll_before == STRIKE:
                 self.frames[self.current_frame - 1] += pins
 
-        if (self.current_frame != 9) and (self.current_roll == STRIKE):
+        if (self.current_frame != FINAL_FRAME) and (self.current_roll == STRIKE):
             # If this is not the final frame, and we have a strike, we
             # end the frame
             self.roll_count = 2
@@ -61,21 +63,21 @@ class BowlingGame(object):
 
         # For frames that are not the final frame, if we have already had 2
         # rolls, reset the roll count to zero and move on to the next frame
-        if self.current_frame != 9:
+        if self.current_frame != FINAL_FRAME:
             if self.roll_count == 2:
                 if self.frames[self.current_frame] > 10:
                     raise ValueError("A frame cannot have more than 10 pts")
                 self.roll_count = 0
                 self.current_frame += 1
         # Edge cases for the final bonus roll in the final frame
-        elif self.current_frame == 9:
+        elif self.current_frame == FINAL_FRAME:
             if self.roll_count == 3:
                 if (self.the_roll_before != STRIKE) and (self.previous_roll != SPARE):
                     raise IndexError("Ten frames already complete")
                 elif (self.the_roll_before == STRIKE) and (self.previous_roll == OPEN):
                     # Subtract the strike value (10) and the previously added
                     # pin value (pins) to get the standing pins
-                    remaining_pins = self.frames[9] - 10 - pins
+                    remaining_pins = self.frames[FINAL_FRAME] - 10 - pins
                     if pins > remaining_pins:
                         raise ValueError("Knocked down pins more than remaining pins")
 
@@ -83,7 +85,7 @@ class BowlingGame(object):
         self.previous_roll = self.current_roll
 
     def score(self):
-        if self.current_frame < 9:
+        if self.current_frame < FINAL_FRAME:
             raise IndexError("Incomplete games cannot be scored")
         elif self.current_roll == STRIKE:
             raise IndexError("Bonus roll for strike has to be rolled first")
