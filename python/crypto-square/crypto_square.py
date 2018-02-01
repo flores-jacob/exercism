5d08b63c8407c66d812b74856ef2e1d4a8f0d1d2
phwebmachine <phwebmachine@gmail.com>
2018-02-01 12:04:49 +0800

    feat: Implement column size computation

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -1,7 +1,17 @@
+from math import ceil
+
 def encode(plain_text):
     valid_chars = "abcdefghijklmnopqrstuvwxyz1234567890"
     lower_case_text = plain_text.lower()
 
     normalized_text = "".join(letter for letter in lower_case_text if letter in valid_chars)
 
-    return normalized_text
+    message_length = len(normalized_text)
+
+    square_root_of_length = message_length ** (1/2.0)
+
+    column_size = ceil(square_root_of_length)
+
+
+
+    return normalized_text.replace(" ", "")
