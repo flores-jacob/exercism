81dc7337b8eb7b24c4c63465a1f6a0b19599caf1
phwebmachine <phwebmachine@gmail.com>
2018-02-01 12:31:28 +0800

    feat: Implement chunking

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -10,8 +10,15 @@ def encode(plain_text):
 
     square_root_of_length = message_length ** (1/2.0)
 
-    column_size = ceil(square_root_of_length)
+    column_size = int(ceil(square_root_of_length))
 
+    row_size = int(ceil(message_length / column_size))
 
+    row_elements = [[] for i in range(column_size)]
+    for index, letter in enumerate(normalized_text):
+        row_elements[index % column_size].append(letter)
 
-    return normalized_text.replace(" ", "")
+    # Pad the last element at the end of the with spaces
+    row_elements[-1].extend(" " * (column_size - len(row_elements[-1])))
+
+    return " ".join("".join(row) for row in row_elements)
