60fef2524a4f8c13a72528ed14e653d3b881dce3
phwebmachine <phwebmachine@gmail.com>
2018-02-05 15:17:02 +0800

    refactor: Use better expressions suited to rust

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -2,13 +2,13 @@
 // especially regarding whether or not code is idiomatic Rust
 
 pub fn raindrops(n: usize) -> String {
-    let mut constructed_string = String::from("");
+    let mut constructed_string = String::new();
 
-    if n % 3 == 0{ constructed_string += "Pling"; };
-    if n % 5 == 0{ constructed_string += "Plang"; };
-    if n % 7 == 0{ constructed_string += "Plong"; };
+    if n % 3 == 0{ constructed_string.push_str("Pling") };
+    if n % 5 == 0{ constructed_string.push_str("Plang") };
+    if n % 7 == 0{ constructed_string.push_str("Plong") };
 
-    if (constructed_string.is_empty()){
+    if constructed_string.is_empty(){
         constructed_string = n.to_string();
     };
 
