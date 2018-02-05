5bc98ca55427b5597240be15d51862e082775036
phwebmachine <phwebmachine@gmail.com>
2018-02-05 15:04:54 +0800

    feat: Adapt C++ code to rust for raindrops exercise

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,16 @@
+// Solution is adapted from my previous C++ code. Open to corrections,
+// especially regarding whether or not code is idiomatic Rust
+
 pub fn raindrops(n: usize) -> String {
-    unimplemented!()
+    let mut constructed_string = String::from("");
+
+    if (n % 3 == 0){ constructed_string += "Pling"; };
+    if (n % 5 == 0){ constructed_string += "Plang"; };
+    if (n % 7 == 0){ constructed_string += "Plong"; };
+
+    if (constructed_string.is_empty()){
+        constructed_string = n.to_string();
+    };
+
+    return constructed_string;
 }
