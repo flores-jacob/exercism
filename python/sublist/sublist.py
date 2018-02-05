8425d818819e5bc21c34719bd57348343227bd8c
phwebmachine <phwebmachine@gmail.com>
2018-02-05 12:46:12 +0800

    feat: Pass all tests, but with atrocious performance

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -1,3 +1,5 @@
+from itertools import islice, zip_longest
+
 SUBLIST = "sublist"
 EQUAL = "equal"
 SUPERLIST = "superlist"
@@ -5,14 +7,18 @@ UNEQUAL = "unequal"
 
 
 def check_lists(first_list, second_list):
-    first_string = " ".join(str(i) for i in first_list)
-    second_string = " ".join(str(j) for j in second_list)
+    # Solution adapted from https://codereview.stackexchange.com/a/149942
+    to_zip1 = ([*islice(second_list, start_index, None)] for start_index in range(len(first_list)))
+    zipped_vals1 = [*zip_longest(*to_zip1)]
+
+    to_zip2 = ([*islice(first_list, start_index, None)] for start_index in range(len(second_list)))
+    zipped_vals2 = [*zip_longest(*to_zip2)]
 
     if first_list == second_list:
         return EQUAL
-    elif first_string in second_string:
+    elif (tuple(first_list) in zipped_vals1) or (first_list == []):
         return SUBLIST
-    elif second_string in first_string:
+    elif (tuple(second_list) in zipped_vals2) or (second_list == []):
         return SUPERLIST
     else:
         return UNEQUAL
