ad9eac413eaa4f3ad0d035bf1cf08310a00c99ca
phwebmachine <phwebmachine@gmail.com>
2018-02-05 14:46:42 +0800

    feat: Implement leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,4 @@
 pub fn is_leap_year(year: i32) -> bool {
-    unimplemented!()
+    // Solution is adapted from my earlier python interpretation
+    (year % 4 == 0) && (! (year % 100 == 0) | (year % 400 == 0))
 }
