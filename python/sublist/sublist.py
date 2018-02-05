bb9e6d198a0589661b82b833daa4f4c69c489e39
phwebmachine <phwebmachine@gmail.com>
2018-02-05 13:11:30 +0800

    refactor: Use more concise and performant code

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -1,5 +1,3 @@
-from itertools import islice, zip_longest
-
 SUBLIST = "sublist"
 EQUAL = "equal"
 SUPERLIST = "superlist"
@@ -7,19 +5,15 @@ UNEQUAL = "unequal"
 
 
 def check_lists(first_list, second_list):
-    # Solution adapted from https://codereview.stackexchange.com/a/149942
-    to_zip1 = ([*islice(second_list, start_index, None)] for start_index in range(len(first_list)))
-    zipped_vals1 = [*zip_longest(*to_zip1)]
-
-    to_zip2 = ([*islice(first_list, start_index, None)] for start_index in range(len(second_list)))
-    zipped_vals2 = [*zip_longest(*to_zip2)]
-
     if first_list == second_list:
         return EQUAL
-    elif (tuple(first_list) in zipped_vals1) or (first_list == []):
-        return SUBLIST
-    elif (tuple(second_list) in zipped_vals2) or (second_list == []):
-        return SUPERLIST
-    else:
-        return UNEQUAL
 
+    for sublist in (second_list[i:i + len(first_list)] for i in range(len(second_list))):
+        if first_list == sublist:
+            return SUBLIST
+
+    for sublist in (first_list[i:i + len(second_list)] for i in range(len(first_list))):
+        if second_list == sublist:
+            return SUPERLIST
+
+    return UNEQUAL
