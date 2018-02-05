508836f55a585ef2cc1c8a11f3e94f1a7e6f1b62
phwebmachine <phwebmachine@gmail.com>
2018-02-05 11:44:44 +0800

    feat: Pass all tests except inner spaces test

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -1,2 +1,19 @@
+SUBLIST = "sublist"
+EQUAL = "equal"
+SUPERLIST = "superlist"
+UNEQUAL = "unequal"
+
+
 def check_lists(first_list, second_list):
-    pass
+    first_string = " ".join(str(i) for i in first_list)
+    second_string = " ".join(str(j) for j in second_list)
+
+    if first_list == second_list:
+        return EQUAL
+    elif first_string in second_string:
+        return SUBLIST
+    elif second_string in first_string:
+        return SUPERLIST
+    else:
+        return UNEQUAL
+
