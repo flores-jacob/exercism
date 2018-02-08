544ea1d23e5a9dc04965299e5c604a9cde3a0445
phwebmachine <phwebmachine@gmail.com>
2018-02-08 13:32:12 +0800

    refactor: Reimplement circular_buffer.py as a deque

--- python/circular-buffer/circular_buffer.py
+++ python/circular-buffer/circular_buffer.py
@@ -13,47 +13,26 @@ class BufferEmptyException(Exception):
 
 class CircularBuffer(object):
     def __init__(self, capacity):
-        self.buffer = [None] * capacity
-        # self.op_queue = deque([0], 5)
-        self.read_index = 0
-        self.write_index = 0
-        self.write_queue = deque([], capacity)
         self.capacity = capacity
+        self.buffer = deque(maxlen=capacity)
 
     def read(self):
-        if self.buffer[self.read_index] is None:
+        try:
+            return self.buffer.pop()
+        except IndexError:
             raise BufferEmptyException
 
-        # Assign data and remove after reading
-        data_val = self.buffer[self.read_index]
-        self.buffer[self.read_index] = None
-
-        # Move to the next index
-        self.read_index = (self.read_index + 1) % self.capacity
-
-        return data_val
-
     def write(self, data):
-        if all(x is not None for x in self.buffer):
+        if len(self.buffer) < self.capacity:
+            self.buffer.appendleft(data)
+        else:
             raise BufferFullException
 
-        self.write_queue.append(self.write_index)
-        self.buffer[self.write_index] = data
-
-        # Move to the next index
-        self.write_index = (self.write_index + 1) % self.capacity
-
     def clear(self):
-        self.buffer = [None] * self.capacity
+        self.buffer.clear()
 
     def overwrite(self, data):
         try:
             self.write(data)
         except BufferFullException:
-            oldest_index = self.write_queue.popleft()
-            self.buffer[oldest_index] = data
-            self.write_queue.append(oldest_index)
-
-            # Move to the next index
-            self.write_index = (oldest_index + 1) % self.capacity
-            self.read_index = (oldest_index + 1) % self.capacity
+            self.buffer.appendleft(data)
