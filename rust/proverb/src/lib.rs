a5a1342164bd884cf00805c763e4aadafe1df09d
phwebmachine <phwebmachine@gmail.com>
2018-02-09 21:47:43 +0800

    feat: Implement Rust proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn build_proverb(list: Vec<&str>) -> String {
-    unimplemented!()
+
+    if list.len() == 0{
+        return String::from("");
+    }
+
+    let mut result_verse_list: Vec<String> = Vec::new();
+
+    for index in 0.. list.len() -1 {
+        result_verse_list.push(format!("For want of a {} the {} was lost.", list[index], list[index + 1]));
+    }
+
+    result_verse_list.push(format!("And all for the want of a {}.", list[0]));
+
+    return String::from(result_verse_list.join("\n"))
 }
