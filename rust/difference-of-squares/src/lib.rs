e8258f62da59ffac10422a43d7d315b95abaad7c
phwebmachine <phwebmachine@gmail.com>
2018-02-09 22:22:10 +0800

    feat: Implement difference-of-squares Rust exercise

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,21 @@
 pub fn square_of_sum(n: usize) -> usize {
-    unimplemented!()
+    let mut total: usize = 0;
+    for i in 1 .. n + 1{
+        total += i;
+    }
+
+    return total.pow(2)
 }
 
 pub fn sum_of_squares(n: usize) -> usize {
-    unimplemented!()
+    let mut total: usize = 0;
+    for i in 1 .. n + 1{
+        total += i.pow(2);
+    }
+
+    return total
 }
 
 pub fn difference(n: usize) -> usize {
-    unimplemented!()
+    return square_of_sum(n) - sum_of_squares(n)
 }
