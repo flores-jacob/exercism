4d9c66699c766acbfad404908cfc493e64137b09
phwebmachine <phwebmachine@gmail.com>
2018-02-12 21:22:42 +0800

    refactor: Make code rustier and more concise

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,18 +1,3 @@
-// Adapted from my python solution to this problem
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    if factors.is_empty() {
-        return 0
-    }else {
-        let mut multiple_sum:u32 = 0;
-
-        for i in 1..limit {
-            for factor in factors {
-                if (i % factor) == 0 {
-                    multiple_sum += i;
-                    break;
-                }
-            }
-        }
-        return multiple_sum
-    }
+    (1..limit).filter(|num| factors.iter().any(|factor| num % factor == 0)).sum()
 }
