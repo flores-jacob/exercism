5158dbe04cd5662696a26916ad9af492aa5d0683
phwebmachine <phwebmachine@gmail.com>
2018-02-12 21:22:05 +0800

    feat: Implement sum-of-multiples rust

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,3 +1,18 @@
+// Adapted from my python solution to this problem
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!()
+    if factors.is_empty() {
+        return 0
+    }else {
+        let mut multiple_sum:u32 = 0;
+
+        for i in 1..limit {
+            for factor in factors {
+                if (i % factor) == 0 {
+                    multiple_sum += i;
+                    break;
+                }
+            }
+        }
+        return multiple_sum
+    }
 }
