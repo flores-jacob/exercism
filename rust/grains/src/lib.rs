b6267c7e335778d3bb4579fe7d0595ef025ed8ee
phwebmachine <phwebmachine@gmail.com>
2018-02-14 23:27:40 +0800

    feat: Implement square for rust grains exercise

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,5 +1,12 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!();
+//    if (s < 1) || (s > 64):
+//        raise ValueError("input square should be between 1 and 64");
+
+    let mut square_val = 1;
+    for i in 2..s + 1 {
+        square_val *= 2
+    }
+    return square_val
 }
 
 pub fn total() -> u64 {
