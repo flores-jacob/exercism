7d392450dc8a468a539752351d83e3e002230066
phwebmachine <phwebmachine@gmail.com>
2018-02-14 22:03:38 +0800

    feat: Implement encode

--- python/rail-fence-cipher/rail_fence_cipher.py
+++ python/rail-fence-cipher/rail_fence_cipher.py
@@ -1,12 +1,31 @@
+from collections import deque
+
+
 def fence_pattern(rails, message_size):
     pass
 
 
 def encode(rails, message):
-    pass
+    deque_list = [deque() for i in range(message)]
+
+    rails = deque(rails)
+    direction = "down"
+    count = 0
+    while rails:
+        mychar = rails.popleft()
+        deque_list[count].append(mychar)
+        if direction == "down":
+            count += 1
+        elif direction == "up":
+            count -= 1
+
+        if count == message - 1:
+            direction = "up"
+        elif count == 0:
+            direction = "down"
+
+    return "".join(["".join(deque_item) for deque_item in deque_list])
 
 
 def decode(rails, encoded_message):
     pass
-
-
