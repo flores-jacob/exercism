43f8267b86fd93fcf8cefb4964b28c55476dee7f
phwebmachine <phwebmachine@gmail.com>
2018-02-16 12:13:37 +0800

    refactor: Simplified pythagorean-triplet for rust

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,12 +1,10 @@
+// More readable solution, without the type wrangling
 pub fn find() -> Option<u32> {
-    for a in 2i32..500{
-        for b in 2i32..500{
-            let c_squared:i32 = a.pow(2u32) + b.pow(2u32);
-            let c:f64 = (c_squared as f64).sqrt();
-            if c == c.round(){
-                if a + b + (c as i32) == 1000{
-                    return Some((a * b * (c as i32)) as u32)
-                }
+    for a in 2..500{
+        for b in 2..500{
+            let c = 1000 - a - b;
+            if a * a + b * b == c * c {
+                return Some(a * b * c)
             }
         }
     }
