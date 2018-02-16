d1f1dc866efaf63d692febfd70cb8bad7879057c
phwebmachine <phwebmachine@gmail.com>
2018-02-16 13:09:55 +0800

    refactor: Put bit shifting in for loop definition

--- cpp/binary/binary.h
+++ cpp/binary/binary.h
@@ -3,18 +3,17 @@
 
 namespace binary {
 	int convert(std::string input){
-		int decimal_value = 0;
-		for (int i=input.length() - 1; i >=0 ; i--){
-			char current_char = input.at(i);
-			// if the current char is neither a 0 or a 1, break and just return 0
-			if ((current_char != '0') && (current_char != '1')){
-				break;
-			};
-			// casting char to int value https://stackoverflow.com/a/30727561
-			int current_int = current_char - '0';
-			const int strlen = input.length();
-			int place_value = 1 << (strlen - (i + 1));
-			decimal_value += current_int * place_value;
+        int place_value = 1;
+        int decimal_value = 0;
+        for (int i=input.length() - 1; i >=0 ; i--, place_value <<= 1){
+            char current_char = input.at(i);
+            // if the current char is neither a 0 or a 1, break and just return 0
+            if ((current_char != '0') && (current_char != '1')){
+                break;
+            };
+            // casting char to int value https://stackoverflow.com/a/30727561
+            int current_int = current_char - '0';
+            decimal_value += current_int * place_value;
 		};
 		return decimal_value;
 	};
