49cad5c49b7a354bdaa3e752370ff48859cec9a0
phwebmachine <phwebmachine@gmail.com>
2018-02-16 13:10:44 +0800

    refactor: Overhaul binary loop structure for c++

--- cpp/binary/binary.h
+++ cpp/binary/binary.h
@@ -7,14 +7,15 @@ namespace binary {
         int decimal_value = 0;
         for (int i=input.length() - 1; i >=0 ; i--, place_value <<= 1){
             char current_char = input.at(i);
-            // if the current char is neither a 0 or a 1, break and just return 0
-            if ((current_char != '0') && (current_char != '1')){
-                break;
-            };
-            // casting char to int value https://stackoverflow.com/a/30727561
-            int current_int = current_char - '0';
-            decimal_value += current_int * place_value;
-		};
+            if (current_char == '1')
+            {
+                decimal_value += place_value;
+            }
+            else if (current_char != '0')
+            {
+                return 0;
+            }
+        };
 		return decimal_value;
 	};
 };
