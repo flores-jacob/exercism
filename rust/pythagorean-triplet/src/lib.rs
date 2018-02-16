eac5067755f19d29ae037378bae35d4fb13a60b4
phwebmachine <phwebmachine@gmail.com>
2018-02-16 12:13:13 +0800

    feat: Implement pythagorean-triplet for rust

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn find() -> Option<u32> {
-    unimplemented!();
+    for a in 2i32..500{
+        for b in 2i32..500{
+            let c_squared:i32 = a.pow(2u32) + b.pow(2u32);
+            let c:f64 = (c_squared as f64).sqrt();
+            if c == c.round(){
+                if a + b + (c as i32) == 1000{
+                    return Some((a * b * (c as i32)) as u32)
+                }
+            }
+        }
+    }
+    return None
 }
