0856da28d7e74f52fc8a3eab23eff0e1356f6cb6
phwebmachine <phwebmachine@gmail.com>
2018-02-19 17:46:07 +0800

    refactor: Simplify code for trinary C++ exercise

--- cpp/trinary/trinary.h
+++ cpp/trinary/trinary.h
@@ -7,11 +7,11 @@ namespace trinary {
 		for (int i=input.length() - 1; i >=0 ; i--){
 			char current_char = input.at(i);
 			// if the current char is neither a 0, 1, or 2, break and just return 0
-			if ((current_char != '0') && (current_char != '1') && (current_char != '2')){
+			if ((current_char < '0') || (current_char > '2')){
 				return 0;
 			};
 			// casting char to int value https://stackoverflow.com/a/30727561
-			int current_int = current_char - 48;
+			int current_int = current_char - '0';
 			int place_value = std::pow(3, abs((i  + 1) - input.length()));
 			decimal_value += current_int * place_value;
 		};
