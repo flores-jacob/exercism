401d5cfeddebb73a0dfbd80c9773102f3f1fcd23
phwebmachine <phwebmachine@gmail.com>
2018-02-19 17:33:28 +0800

    refactor: Simplify and speed up code

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -7,21 +7,13 @@ def nth_prime(positive_number):
 
         # while we have not yet discovered up to n prime numbers
         while len(discovered_primes) < positive_number:
-            # assume that current int is not divisible
-            divisible = False
-            # we loop through each of the primes we have already discovered
-            for prime in discovered_primes:
-                # we check if the current int in question is divisible by any of the already discovered ones
-                if current_int % prime == 0:
-                    divisible = True
-                    # if it is divisible, then we increase the int count by one
-                    current_int += 1
-                    # and we move on to the next item in the while loop
-                    break
 
-            # if current int is not divisible by any of the previously discovered primes
-            # then we add it to the discovered primes vector, and increase current_prime_count by one
-            if divisible is False:
+            # If all of the currently discovered primes cannot evenly
+            # divide the current_int, then we add current_int as a prime
+            if all(current_int % prime != 0 for prime in discovered_primes):
                 discovered_primes.append(current_int)
+
+            current_int += 1
+
         return discovered_primes[-1]
 
