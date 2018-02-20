4b4ccd1297bcb8a8a717c6609a868c06ef4a346c
phwebmachine <phwebmachine@gmail.com>
2018-02-20 20:44:56 +0800

    feat: Implement saddle-points python exercise

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,14 @@
 def saddle_points(matrix):
-    pass
+    saddle_point_list = []
+    for row_index, row in enumerate(matrix):
+        for col_index, element in enumerate(row):
+
+            try:
+                col_vals = [row2[col_index] for row2 in matrix]
+            except IndexError:
+                raise ValueError("Irregular matrix")
+
+            if (element == max(row)) and (element == min(col_vals)):
+                saddle_point_list.append((row_index, col_index))
+
+    return set(saddle_point_list)
