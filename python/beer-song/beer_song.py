384bdc1bc80f47c71ea4bcf7327b326a43913883
phwebmachine <phwebmachine@gmail.com>
2018-02-23 02:06:07 +0800

    feat: Write generate_generic_verse function

--- python/beer-song/beer_song.py
+++ python/beer-song/beer_song.py
@@ -1,2 +1,9 @@
+def generate_generic_verse(verse_number):
+    return (["{} bottles of beer on the wall, {} bottles of beer.".format(verse_number, verse_number),
+             "Take one down and pass it around, {} bottles of beer on the wall.".format(verse_number - 1)])
+
+
 def recite(start, take=1):
-    pass
+    song = generate_generic_verse(start)
+    print(song)
+    return (song)
