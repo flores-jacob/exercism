2e9a4c6f72a738a88fefe761e00bb68e0f6dd225
phwebmachine <phwebmachine@gmail.com>
2018-02-24 14:05:21 +0800

    refactor: Reduce memory consumption and speed up computation

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,21 +1,22 @@
-def get_factors(natural_number):
-    factor_list = []
-    candidate_factor = 1
-
-    while candidate_factor < natural_number:
-        if natural_number % candidate_factor == 0:
-            factor_list.append(candidate_factor)
-        candidate_factor += 1
-
-    return factor_list
-
-
 def classify(number):
 
     if number <= 0:
         raise ValueError("Input number should be positive")
+    elif number == 1:
+        return "deficient"
 
-    aliquot_sum = sum(get_factors(number))
+    aliquot_sum = 1
+    candidate_factor = 2
+    candidate_factor_pair = number
+
+    while candidate_factor < candidate_factor_pair:
+        if number % candidate_factor == 0:
+            candidate_factor_pair = int(number/candidate_factor)
+
+            aliquot_sum += candidate_factor
+            if candidate_factor != candidate_factor_pair:
+                aliquot_sum += candidate_factor_pair
+        candidate_factor += 1
 
     if aliquot_sum == number:
         return "perfect"
