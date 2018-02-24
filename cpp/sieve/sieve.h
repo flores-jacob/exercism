14da0d5cc31a4b336713024eb40929b309d7d716
phwebmachine <phwebmachine@gmail.com>
2018-02-24 21:04:34 +0800

    refactor: Greatly reduce memory consumption and execution time

--- cpp/sieve/sieve.h
+++ cpp/sieve/sieve.h
@@ -5,26 +5,44 @@ namespace sieve
     std::vector<int> primes(int n)
     {
 
-        // create a list of numbers up to the input, starting from the number 2
+        // Create a list of numbers up to the input, starting from the number 2
         std::vector<int> integer_list;
         for (int i=2; i<=n; i++){
             integer_list.push_back(i);
         };
 
-        // iterate through the list for each element as a divisor
-        for (int divisor: integer_list){
-            // if any other element in the list can be divided by the divisor:
-            for (int elem: integer_list){
-                // if the integer is divisible by the current divisor, and the divisor is not the same as the integer
-                // as the number, then it is not a prime number and we remove it from the vector
-                if ((elem % divisor == 0) && (elem != divisor)){
-                    integer_list.erase(std::remove(integer_list.begin(), integer_list.end(), elem), integer_list.end());
-                };
+        // Create a list of bool values that map to each number in the integer list
+        std::vector<bool> is_prime_list(n, true);
+
+        // Loop through the list of integers as divisors using their indices.
+        // Loop a second time through the list to check mark off any numbers
+        // that are divisible by these divisors
+
+        // We start from zero, and end at n-2, because the integer_list starts
+        // with the number 2; 0 and 1 are not in the list
+        for (int divisor_index=0; divisor_index <= n-2; divisor_index++){
+            int divisor_val = integer_list.at(divisor_index);
+            // Loop once again through the list of integers as dividends.
+            // Starting with the value of the current divisor + divisor_val,
+            // we increment by the value of the divisor, and all dividends
+            // found this way are divisible by the divisor and are non-prime
+            for (int dividend_index=divisor_index + divisor_val; dividend_index <= n-2; dividend_index += divisor_val){
+                is_prime_list.at(dividend_index) = false;
             };
         };
 
-        // return the modified list
-        return integer_list;
+        // Prepare the output_list
+        std::vector<int> output_list;
+
+        // Loop through the is_prime_list; if the element is true, then
+        // it maps to a prime in the integer_list
+        for (int i=0; i <= n - 2; i++){
+            if (is_prime_list.at(i) == true){
+                output_list.push_back(integer_list.at(i));
+            }
+        }
+
+        return output_list;
     };
 
 };
