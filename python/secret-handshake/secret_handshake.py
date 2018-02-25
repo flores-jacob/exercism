13f4c8eaa64ae95b2c8287d440fa97be4651290d
phwebmachine <phwebmachine@gmail.com>
2018-02-26 01:20:00 +0800

    feat: Implement handshake()

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,6 +1,22 @@
 def handshake(code):
-    pass
+    # https://stackoverflow.com/a/699891
+    bin_str = "{0:b}".format(code)
 
+    action_list = []
+
+    for index, num in enumerate(reversed(bin_str)):
+        if num == '1':
+            if index == 0:
+                action_list.append("wink")
+            elif index == 1:
+                action_list.append("double blink")
+            elif index == 2:
+                action_list.append("close your eyes")
+            elif index == 3:
+                action_list.append("jump")
+            elif index == 4:
+                action_list.reverse()
+    return action_list
 
 def secret_code(actions):
     pass
