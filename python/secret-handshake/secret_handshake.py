1ca3e4d1479631353da4ff19670ae84e84430126
phwebmachine <phwebmachine@gmail.com>
2018-02-26 01:44:27 +0800

    feat: Implement secret_code()

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -18,5 +18,24 @@ def handshake(code):
                 action_list.reverse()
     return action_list
 
+
 def secret_code(actions):
-    pass
+    vals = {"wink": int('1', 2),
+            "double blink": int('10', 2),
+            "close your eyes": int('100', 2),
+            "jump": int('1000', 2)
+            }
+
+    total = 0
+
+    for action in actions:
+        total += vals[action]
+
+    # Check if reversed
+    # If the number of actions are more than one
+    # And if the value of the first element is greater than the last
+    # element, then the list is reversed, and we add b10,000
+    if (len(actions) > 1) and (vals[actions[0]] > vals[actions[-1]]):
+        total += int('10000', 2)
+
+    return total
