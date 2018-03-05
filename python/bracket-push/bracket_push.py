49ec51b88c062226b7921574c82f460c385f6130
phwebmachine <phwebmachine@gmail.com>
2018-03-05 12:03:15 +0800

    docs: Clarify bracket_push.py code with comments

--- python/bracket-push/bracket_push.py
+++ python/bracket-push/bracket_push.py
@@ -5,19 +5,31 @@ def brackets_match(opening_bracket, closing_bracket):
 
 def is_paired(input_string):
     bracket_stack = []
-
+    # Add every "opening" bracket to the stack
+    # if we encounter a "closing" bracket, it should match the current
+    # bracket on top of the stack
     for char in input_string:
+        # If we encounter an opening bracket, we add it on top of the stack
         if (char == "(") or (char == "[") or (char == "{"):
             bracket_stack.append(char)
         elif (char == ")") or (char == "]") or (char == "}"):
+            # If the bracket_stack is empty, and we start with a closing
+            # bracket, then we return false, as the first bracket has no
+            # pair
             if len(bracket_stack) == 0:
                 return False
 
+            # Get the bracket on top of the stack
             top_of_stack = bracket_stack.pop()
 
+            # Check and return if the opening and closing brackets match
             return brackets_match(top_of_stack, char)
 
+    # Once done processing the string, and there are unmatched brackets,
+    # then the brackets are asymmetrical and we return false
     if len(bracket_stack) > 0:
         return False
 
+    # If the checks above did not detect any irregularities, then all
+    # the brackets match, and we return true
     return True
