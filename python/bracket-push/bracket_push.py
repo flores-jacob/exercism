593ebc40d9b1970c2e0a95e297d227ac4529c824
phwebmachine <phwebmachine@gmail.com>
2018-03-05 11:57:47 +0800

    feat: Implement bracket_push.py

--- python/bracket-push/bracket_push.py
+++ python/bracket-push/bracket_push.py
@@ -1,2 +1,23 @@
+def brackets_match(opening_bracket, closing_bracket):
+    bracket_dict = {"(": ")", "[": "]", "{": "}"}
+    return bracket_dict[opening_bracket] == closing_bracket
+
+
 def is_paired(input_string):
-    pass
+    bracket_stack = []
+
+    for char in input_string:
+        if (char == "(") or (char == "[") or (char == "{"):
+            bracket_stack.append(char)
+        elif (char == ")") or (char == "]") or (char == "}"):
+            if len(bracket_stack) == 0:
+                return False
+
+            top_of_stack = bracket_stack.pop()
+
+            return brackets_match(top_of_stack, char)
+
+    if len(bracket_stack) > 0:
+        return False
+
+    return True
