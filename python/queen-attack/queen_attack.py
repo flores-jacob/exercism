1c8584ba8f2301d436a5fdba4b1fb2a3248f757c
phwebmachine <phwebmachine@gmail.com>
2018-03-14 14:05:41 +0800

    feat: Detect if queen can attack queen with same row or column

--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -1,6 +1,14 @@
 class Queen(object):
     def __init__(self, row, column):
-        pass
+        if (0 <= row <= 7) and (0 <= column <= 7):
+            self.row = row
+            self.column = column
+        else:
+            raise ValueError("Invalid row and column")
 
     def can_attack(self, another_queen):
-        pass
+
+        if (self.row == another_queen.row) or (self.column == another_queen.column):
+            return True
+        else:
+            return False
