39a14ab7e05636e529cb345466f7a2d973a18a30
phwebmachine <phwebmachine@gmail.com>
2018-03-19 08:15:42 +0800

    feat: Implement addition, subtraction, multiplication, division

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,2 +1,35 @@
 def calculate(question):
-    pass
+    split_question = question.split()
+
+    answer = None
+
+    for index, word in enumerate(split_question):
+        if (word == "plus") or (word == "minus"):
+            num1 = split_question[index - 1]
+            num2 = split_question[index + 1].rstrip("?")
+
+            if word == "plus":
+                if answer:
+                    answer += int(num2)
+                else:
+                    answer = int(num1) + int(num2)
+            elif word == "minus":
+                if answer:
+                    answer -= int(num2)
+                else:
+                    answer = int(num1) - int(num2)
+        elif (word == "multiplied") or (word == "divided"):
+            num1 = split_question[index - 1]
+            num2 = split_question[index + 2].rstrip("?")
+            if word == "multiplied":
+                if answer:
+                    answer *= int(num2)
+                else:
+                    answer = int(num1) * int(num2)
+            elif word == "divided":
+                if answer:
+                    answer /= int(num2)
+                else:
+                    answer = int(num1) / int(num2)
+
+    return answer
