402beb13db4a0ffe78608e2e851e0b429112c130
phwebmachine <phwebmachine@gmail.com>
2018-03-19 09:19:03 +0800

    fix: Pass missing operator test

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,9 +1,16 @@
 def calculate(question):
     split_question = question.split()
 
+    # Check if operator is missing
+
     answer = None
+    operator_count = 0
+    operand_count = 0
 
     for index, word in enumerate(split_question):
+        if word.lstrip("-").rstrip("?").isdigit():
+            operand_count += 1
+
         if (word == "plus") or (word == "minus"):
             offset = 1
         elif (word == "multiplied") or (word == "divided"):
@@ -14,6 +21,7 @@ def calculate(question):
             offset = None
 
         if offset:
+            operator_count += 1
             num1 = split_question[index - 1]
             num2 = split_question[index + offset].rstrip("?").rstrip("st").rstrip("nd").rstrip("rd").rstrip("th")
 
@@ -43,7 +51,9 @@ def calculate(question):
                 else:
                     answer = int(num1) ** int(num2)
 
-    if answer is not None:
+    if operand_count != operator_count + 1:
+        raise ValueError("missing operator")
+    elif answer is not None:
         return answer
     else:
         raise ValueError("Improper question")
