572d4e45f99711d8f8a5de9e0fd5f4cc186138b6
phwebmachine <phwebmachine@gmail.com>
2018-03-20 12:18:16 +0800

    fix: Enable area code and exchange code checking

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -6,8 +6,11 @@ class Phone(object):
         # Check number length. if length is 11, and number begins with country code 1, then return last 10 numbers
         if (len(number_list) == 11) and number_list[0] == "1":
             number_list = number_list[1:]
-        # If number_length is incorrect, or if the area code is incorrect, raise an error
-        elif (len(number_list) != 10) or (number_list[0] not in [1, 2, 3, 4, 5, 6, 7, 8, 9]):
+        # If number_length is incorrect, or if the area code or exchange
+        # code is incorrect, raise an error
+        elif (len(number_list) != 10) \
+                or (number_list[0] in "01") \
+                or (number_list[3] in "01"):
             raise ValueError("Invalid phone number")
 
         self.number = "".join(number_list)
