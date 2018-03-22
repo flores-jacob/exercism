feac390f779c1ed41cbd68505169af9dc42b64fe
phwebmachine <phwebmachine@gmail.com>
2018-03-22 20:24:32 +0800

    feat: Enable support for multiple numbers in horizontal row

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -1,5 +1,4 @@
 def convert(input_grid):
-
     if len(input_grid) % 4 != 0:
         raise ValueError("Number of input lines not multiple of 4")
 
@@ -7,9 +6,7 @@ def convert(input_grid):
         if len(row) % 3 != 0:
             raise ValueError("Row numbers not multiple of 3")
 
-    chunk = input_grid
-
-    chunk_list = [
+    chunk_reference = [
         [" _ ",
          "| |",
          "|_|",
@@ -20,11 +17,31 @@ def convert(input_grid):
          "   "]
     ]
 
-    output = "?"
+    num_chunks = int(len(input_grid[0]) / 3)
 
-    for index, chunk_candidate in enumerate(chunk_list):
-        if chunk_candidate == chunk:
-            output = str(index)
+    chunk_list = []
 
-    return output
+    for num_chunk in range(num_chunks):
 
+        beginning_index = 3 * num_chunk
+        end_index = beginning_index + 3
+
+        chunk = [
+            "".join([char for char in input_grid[0][beginning_index:end_index]]),
+            "".join([char for char in input_grid[1][beginning_index:end_index]]),
+            "".join([char for char in input_grid[2][beginning_index:end_index]]),
+            "".join([char for char in input_grid[3][beginning_index:end_index]])
+        ]
+
+        print(chunk)
+
+        chunk_val = "?"
+
+        for index, chunk_candidate in enumerate(chunk_reference):
+            if chunk_candidate == chunk:
+                chunk_val = str(index)
+                break
+
+        chunk_list.append(chunk_val)
+
+    return "".join(chunk_list)
