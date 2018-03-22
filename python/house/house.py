bf8831e0d786b7c4d57aaf77b8fa79122e5658ec
phwebmachine <phwebmachine@gmail.com>
2018-03-22 22:11:18 +0800

    refactor: Rename several items for clarity

--- python/house/house.py
+++ python/house/house.py
@@ -1,26 +1,25 @@
-from collections import OrderedDict, namedtuple
+from collections import namedtuple
 
 base_verse = "This is the house that Jack built."
 
-Pair = namedtuple('Pair', ['subject', 'action'])
+SubjectActionPair = namedtuple('SubjectActionPair', ['subject', 'action'])
 
 pair_list = [
-    Pair('malt', 'lay in'),
-    Pair('rat', 'ate'),
-    Pair('cat', 'killed'),
-    Pair('dog', 'worried'),
-    Pair('cow with the crumpled horn', 'tossed'),
-    Pair('maiden all forlorn', 'milked'),
-    Pair('man all tattered and torn', 'kissed'),
-    Pair('priest all shaven and shorn', 'married'),
-    Pair('rooster that crowed in the morn', 'woke'),
-    Pair('farmer sowing his corn', 'kept'),
-    Pair('horse and the hound and the horn', 'belonged to'),
-
+    SubjectActionPair('malt', 'lay in'),
+    SubjectActionPair('rat', 'ate'),
+    SubjectActionPair('cat', 'killed'),
+    SubjectActionPair('dog', 'worried'),
+    SubjectActionPair('cow with the crumpled horn', 'tossed'),
+    SubjectActionPair('maiden all forlorn', 'milked'),
+    SubjectActionPair('man all tattered and torn', 'kissed'),
+    SubjectActionPair('priest all shaven and shorn', 'married'),
+    SubjectActionPair('rooster that crowed in the morn', 'woke'),
+    SubjectActionPair('farmer sowing his corn', 'kept'),
+    SubjectActionPair('horse and the hound and the horn', 'belonged to'),
 ]
 
 
-def _construct_stanza(stanza_num_input):
+def construct_stanza(stanza_num_input):
 
     constructed_stanza = base_verse
 
@@ -36,7 +35,7 @@ def recite(start_verse, end_verse):
     song = []
 
     for i in range(start_verse - 1, end_verse):
-        song.extend(_construct_stanza(i))
+        song.extend(construct_stanza(i))
         song.append("")
 
     return song[:-1]
