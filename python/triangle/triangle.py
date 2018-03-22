6c10853e42704a2e9cab3fc99dd220125639abdc
phwebmachine <phwebmachine@gmail.com>
2018-03-23 07:03:37 +0800

    feat: Implement is_isosceles

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,12 +1,26 @@
-def is_equilateral(sides):
+def is_valid_triangle(sides):
     if any(sides) <= 0:
         return False
+    elif not((sides[0] + sides[1]) >= sides[2]) \
+            or not ((sides[0] + sides[2]) >= sides[1]) \
+            or not ((sides[1] + sides[2]) >= sides[0]):
+        return False
     else:
+        return True
+
+
+def is_equilateral(sides):
+    if is_valid_triangle(sides):
         return (sides[0] == sides[1]) and (sides[1] == sides[2])
+    else:
+        return False
 
 
 def is_isosceles(sides):
-    pass
+    if is_valid_triangle(sides):
+        return (sides[0] == sides[1]) or (sides[0] == sides[2]) or (sides[1] == sides[2])
+    else:
+        return False
 
 
 def is_scalene(sides):
