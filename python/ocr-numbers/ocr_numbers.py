dfdfc505a8c60045fe4485363f9ca7f1e69a8a65
phwebmachine <phwebmachine@gmail.com>
2018-03-22 20:01:54 +0800

    feat: Enable for single digits

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -1,2 +1,23 @@
 def convert(input_grid):
-    pass
+
+    chunk = input_grid
+
+    chunk_list = [
+        [" _ ",
+         "| |",
+         "|_|",
+         "   "],
+        ["   ",
+         "  |",
+         "  |",
+         "   "]
+    ]
+
+    output = "?"
+
+    for index, chunk_candidate in enumerate(chunk_list):
+        if chunk_candidate == chunk:
+            output = str(index)
+
+    return output
+
