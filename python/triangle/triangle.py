c57e4090fc778656e7e077ab5d0670f78ac9f0bd
phwebmachine <phwebmachine@gmail.com>
2018-03-23 06:55:26 +0800

    feat: Implement is_equilateral

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,5 +1,8 @@
 def is_equilateral(sides):
-    pass
+    if any(sides) <= 0:
+        return False
+    else:
+        return (sides[0] == sides[1]) and (sides[1] == sides[2])
 
 
 def is_isosceles(sides):
