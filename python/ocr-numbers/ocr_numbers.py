f8e627f9b93716ca8c8585c9f8d0ecc8e60fdb6a
phwebmachine <phwebmachine@gmail.com>
2018-03-22 20:06:48 +0800

    feat: Raise error if number of columns is not divisible by 3

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -3,6 +3,10 @@ def convert(input_grid):
     if len(input_grid) % 4 != 0:
         raise ValueError("Number of input lines not multiple of 4")
 
+    for row in input_grid:
+        if len(row) % 3 != 0:
+            raise ValueError("Row numbers not multiple of 3")
+
     chunk = input_grid
 
     chunk_list = [
