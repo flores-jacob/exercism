ffaea096c1654c82bed4878fb5b078396bf721a5
phwebmachine <phwebmachine@gmail.com>
2018-03-23 08:26:29 +0800

    refactor: Use simplified algorithm

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,18 +1,18 @@
 pub fn factors(n:i64) -> Vec<i64>{
 
     let mut prime_factors:Vec<i64> = vec![];
+    let mut candidate_factor: i64 = 2;
+    let mut dividend: i64 = n;
 
-    let mut input: i64 = n;
-
-    for divisor in 2..n + 1 {
-        if input % divisor == 0 {
-            prime_factors.push(divisor);
-            input = input/divisor;
-            // recursively get factors of updated input
-            prime_factors.extend(factors(input));
-            break;
-        };
-    };
+    while candidate_factor <= dividend{
+        if dividend % candidate_factor == 0 {
+            prime_factors.push(candidate_factor);
+            dividend /= candidate_factor;
+        }
+        else {
+            candidate_factor += 1;
+        }
+    }
 
     return prime_factors
 }
