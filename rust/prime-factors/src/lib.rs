540db541a702ccdd0c9b1898ace3a0685b5f2034
phwebmachine <phwebmachine@gmail.com>
2018-03-23 08:08:55 +0800

    feat: Pass first 2 tests

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn factors(n:i64) -> Vec<i64>{
-    vec![]
+
+    let mut prime_factors:Vec<i64> = vec![];
+
+    let mut current_num: i64 = n;
+
+    for divisor in 2..n + 1 {
+        if current_num % divisor == 0 {
+            prime_factors.push(divisor);
+            current_num = current_num/divisor;
+
+            println!("factors are {:?}", prime_factors);
+        }
+
+    };
+
+    return prime_factors
 }
