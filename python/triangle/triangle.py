cdd679d20602af7b2c3d8620d52a7c82fb40419c
phwebmachine <phwebmachine@gmail.com>
2018-03-24 16:37:36 +0800

    refactor: Simplify code using decorator

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,31 +1,25 @@
-def is_valid_triangle(sides):
-    if any(sides) <= 0:
-        return False
-    elif not((sides[0] + sides[1]) >= sides[2]) \
-            or not ((sides[0] + sides[2]) >= sides[1]) \
-            or not ((sides[1] + sides[2]) >= sides[0]):
-        return False
-    else:
-        return True
+def is_valid_triangle(func):
+    def inner(sides):
+        if any(sides) <= 0:
+            return False
+        elif not((sides[0] + sides[1]) >= sides[2]) \
+                or not ((sides[0] + sides[2]) >= sides[1]) \
+                or not ((sides[1] + sides[2]) >= sides[0]):
+            return False
+        return func(sides)
+    return inner
 
 
+@is_valid_triangle
 def is_equilateral(sides):
-    return is_valid_triangle(sides) \
-           and (sides[0] == sides[1]) \
-           and (sides[1] == sides[2])
+    return (sides[0] == sides[1]) and (sides[1] == sides[2])
 
 
+@is_valid_triangle
 def is_isosceles(sides):
-    return is_valid_triangle(sides) \
-           and (
-                   (sides[0] == sides[1])
-                   or (sides[0] == sides[2])
-                   or (sides[1] == sides[2])
-           )
+    return (sides[0] == sides[1]) or (sides[0] == sides[2]) or (sides[1] == sides[2])
 
 
+@is_valid_triangle
 def is_scalene(sides):
-    return is_valid_triangle(sides) \
-           and (sides[0] != sides[1]) \
-           and (sides[0] != sides[2]) \
-           and (sides[1] != sides[2])
+    return (sides[0] != sides[1]) and (sides[0] != sides[2]) and (sides[1] != sides[2])
