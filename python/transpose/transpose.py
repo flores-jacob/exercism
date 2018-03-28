d594da56d790a4c0d2b8e98b45aa06f102b37a0f
phwebmachine <phwebmachine@gmail.com>
2018-03-28 11:15:27 +0800

    feat: Enable handling of matrices with same length rows

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,2 +1,12 @@
 def transpose(input_lines):
-    pass
+
+    output = []
+
+    for input_row_index, input_row in enumerate(input_lines.split("\n")):
+        for char_index, char in enumerate(input_row):
+            try:
+                output[char_index].append(char)
+            except IndexError:
+                output.append([char])
+
+    return "\n".join(["".join(output_row) for output_row in output])
