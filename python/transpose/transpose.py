1e7b5be5a244e3410ec5d9165bbfcef7b681f4bf
phwebmachine <phwebmachine@gmail.com>
2018-03-28 12:34:02 +0800

    feat: Pass all tests

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,12 +1,34 @@
+
+
 def transpose(input_lines):
+    input_rows = input_lines.split("\n")
+
+    max_row_len = max((len(row) for row in input_rows))
 
     output = []
 
-    for input_row_index, input_row in enumerate(input_lines.split("\n")):
-        for char_index, char in enumerate(input_row):
+    for input_row in input_rows:
+        for index in range(max_row_len):
             try:
-                output[char_index].append(char)
+                char = input_row[index]
+            except IndexError:
+                char = " "
+
+            try:
+                output[index].append(char)
             except IndexError:
                 output.append([char])
 
-    return "\n".join(["".join(output_row) for output_row in output])
+    concatenated_rows = ["".join(output_row) for output_row in output]
+
+    current_max_row_len = 0
+
+    for i in range(len(concatenated_rows) - 1, -1, -1):
+        current_row_len = len(concatenated_rows[i].rstrip())
+
+        if current_row_len > current_max_row_len:
+            current_max_row_len = current_row_len
+
+        concatenated_rows[i] = concatenated_rows[i].rstrip() + (" " * (current_max_row_len - current_row_len))
+
+    return "\n".join(concatenated_rows)
